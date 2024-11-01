d6f5a963f8f62558443e317d117ab6ceea0a0dde
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-10-31 23:02:55 -0400

    Add container style for main application layout
    - Created .containerThreeComponents in component.css to define a unified background and layout for the CV application.
    - Wrapped GeneralInfo, EducationInfo, and ExperienceInfo components within the new container in page.js for better styling and structure.

--- app/page.js
+++ app/page.js
@@ -1,14 +1,21 @@
 import GeneralInfo from "@/app/component/GeneralInfo";
 import EducationInfo from "@/app/component/EducationInfo";
 import ExperienceInfo from "@/app/component/ExperienceInfo";
+import './style/Components.css';
+
 
 export default function Home() {
+
+
     return (
-      <div>
-          <h1>CV application</h1>
-          <GeneralInfo/>
-          <EducationInfo/>
-          <ExperienceInfo/>
-      </div>
+        <>
+
+            <div className="containerThreeComponents">
+                <h1>CV application</h1>
+                <GeneralInfo/>
+                <EducationInfo/>
+                <ExperienceInfo/>
+            </div>
+        </>
     );
-}
+};
